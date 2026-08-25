eda81c4fa7ea64a64aee3420c4674104d8cf806d
Darma Wiranda <darmawiranda506@gmail.com>
2026-08-26 02:37:14 +0700

    Fix Android compile SDK for release build

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 
 android {
     namespace = "com.example.tianride"
-    compileSdk = flutter.compileSdkVersion
+    compileSdk = 36
     ndkVersion = flutter.ndkVersion
 
     compileOptions {
