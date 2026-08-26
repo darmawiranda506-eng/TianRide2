4e3d3c3ed7079767ca201157ed07c5a67e2e734f
Darma Wiranda <darmawiranda506@gmail.com>
2026-08-26 17:54:44 +0700

    Setup GitHub Actions APK build

--- android/settings.gradle.kts
+++ android/settings.gradle.kts
@@ -4,7 +4,9 @@ pluginManagement {
             val properties = java.util.Properties()
             file("local.properties").inputStream().use { properties.load(it) }
             val flutterSdkPath = properties.getProperty("flutter.sdk")
-            require(flutterSdkPath != null) { "flutter.sdk not set in local.properties" }
+            require(flutterSdkPath != null) {
+                "flutter.sdk not set in local.properties"
+            }
             flutterSdkPath
         }
 
@@ -20,6 +22,7 @@ pluginManagement {
 plugins {
     id("dev.flutter.flutter-plugin-loader") version "1.0.0"
     id("com.android.application") version "9.2.0" apply false
+    id("org.jetbrains.kotlin.android") version "2.2.20" apply false
     id("com.google.gms.google-services") version "4.4.2" apply false
 }
 
